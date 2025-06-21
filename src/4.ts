82b945d86f70d99157fdf80f94198b82c5e2bb70
AlexSimakhin <oleksandrsimakhin@gmail.com>
2025-06-21 12:57:01 +0300

    add: Added HW

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,64 @@
-const key = new Key();
+class Key {
+  private signature: number;
 
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  public getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  public getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  private tenants: Person[] = [];
+  protected key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  public comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log('Person has entered the house.');
+    } else {
+      console.log('The door is closed.');
+    }
+  }
+
+  public abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  public openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log('The door is now open.');
+    } else {
+      console.log('Wrong key. The door stays closed.');
+    }
+  }
+}
+
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
-
 house.comeIn(person);
-
 
 export {};
